fe8f96888b75c336ff807d640d50d2ab461d7377
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-11-29 18:20:42 +0300

    Добавил новый проект Example005_Random

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine("Первое число: " + numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine("Второе число: " + numberB);
+int result = numberA + numberB;
+Console.WriteLine("Сумма двух чисел равна:  " + result);
+
